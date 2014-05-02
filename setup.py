9286ed536dced58209f2c290290378e3a125221f
podhmo <podhmo+altair@beproud.jp>
2014-05-02 23:26:08 +0900

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requires = [
     ]
 
 setup(name='zca_snippets',
-      version='0.0.1',
+      version='0.0.2',
       description='snippets for zope.interface',
       long_description="", 
       author='podhmo',
